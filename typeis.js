49ce06b6b24d991e26144550ecf4b7ac5940cb1e
liou <peng820401@outlook.com>
2019-12-17 19:18:28 +0800

    v1

---
+++ typeis.js
@@ -0,0 +1,14 @@
+
+ function typeis(parameter){
+    let type=typeof(parameter)
+    if(type==="object") return objectType(parameter)
+    else return type
+}
+
+function objectType(parameter){
+    if(parameter===null) return 'null'
+    if(Array.isArray(parameter)==true) return "array"
+    if(parameter instanceof RegExp) return "RegExp"
+    if(parameter instanceof Date)return "Date"
+}
+export {typeis}
